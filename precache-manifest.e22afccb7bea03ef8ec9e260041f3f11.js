0273972ccb427d68c98692a6ee7620a58e22f668
Michael <michaelzhaojq@hotmail.com>
2020-04-05 17:03:53 +1200

    Updates

---
+++ precache-manifest.e22afccb7bea03ef8ec9e260041f3f11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf5d84bc68ac3b877f8e49cd78296e9",
+    "url": "/web-app/index.html"
+  },
+  {
+    "revision": "847ec82a7d9b15cf1aba",
+    "url": "/web-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "27fde765fcea9d23514a",
+    "url": "/web-app/static/js/2.c9553c95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-app/static/js/2.c9553c95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "847ec82a7d9b15cf1aba",
+    "url": "/web-app/static/js/main.0229f620.chunk.js"
+  },
+  {
+    "revision": "89dccc91abffe9a248ae",
+    "url": "/web-app/static/js/runtime-main.72e96756.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
